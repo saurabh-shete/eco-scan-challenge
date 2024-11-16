ec31c6423de0b160e2ced36183e75c80906dbdd0
Saurabh Shete <ss.saurabhshete@gmail.com>
2024-11-16 13:34:20 +0530

    Initial commit

---
+++ ecoscanapk/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
