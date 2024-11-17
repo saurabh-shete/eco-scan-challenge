339e02ff19aed53973f0f98e8d0342e088228316
Saurabh Shete <ss.saurabhshete@gmail.com>
2024-11-17 19:03:46 +0530

    fix: backend url changed

--- ecoscanapk/src/config/index.js
+++ ecoscanapk/src/config/index.js
@@ -1,3 +1,4 @@
-const BACKEND_URL = 'http://192.168.1.15:8000';
+// const BACKEND_URL = 'http://192.168.0.106:8000';
+const BACKEND_URL = 'https://eco-scan-challenge-bgwb.onrender.com';
 
 export default BACKEND_URL;
