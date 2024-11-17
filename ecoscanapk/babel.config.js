813e7726c8076f19bb59e2f0c62cbd0d8ec14787
Saurabh Shete <ss.saurabhshete@gmail.com>
2024-11-18 02:31:43 +0530

    fix: made some changes to run on android

--- ecoscanapk/babel.config.js
+++ ecoscanapk/babel.config.js
@@ -10,5 +10,8 @@ module.exports = {
         allowUndefined: true,
       },
     ],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
   ],
 };
